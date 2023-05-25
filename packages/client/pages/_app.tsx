72b4049ff4ffe88a575483c4bfe4dc53fa437c26
nahom19 <mailtonahom2@gmail.com>
2023-05-25 16:31:11 +0300

    new: add hello world message

--- packages/client/pages/_app.tsx
+++ packages/client/pages/_app.tsx
@@ -1,12 +1,11 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
-import './styles.css';
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Welcome to client!</title>
+        <title>Url Shortener</title>
       </Head>
       <main className="app">
         <Component {...pageProps} />
