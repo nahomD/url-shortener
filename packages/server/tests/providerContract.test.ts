855a8c9c519f9a4b05833e12aeeb0fbc09209ed9
nahom19 <mailtonahom2@gmail.com>
2023-05-29 13:36:12 +0300

    test(server): add Pact provider test

---
+++ packages/server/tests/providerContract.test.ts
@@ -0,0 +1,32 @@
+import { Verifier } from '@pact-foundation/pact';
+import app from '../src/app';
+import path from 'path';
+
+const port = 8081;
+const baseUrl = `http://localhost:${port}`;
+
+const server = app.listen(port, () => {
+  console.log(`Service listening on ${baseUrl}`);
+});
+
+describe('Pact verification', () => {
+  test('validate the expectations of the matching consumer', () => {
+    return new Verifier({
+      providerBaseUrl: baseUrl,
+      pactUrls: [
+        path.resolve(
+          process.cwd(),
+          './pacts/urlShortener-client-urlShortener-server.json'
+        ),
+      ],
+    })
+      .verifyProvider()
+      .then(() => {
+        console.log('Pact Verification Complete!');
+      });
+  });
+
+  afterAll(() => {
+    server.close();
+  });
+});
