4e82432fe3dd2f5dd5e3f4d2c53843003ab93a9a
nahom19 <mailtonahom2@gmail.com>
2023-05-30 12:24:32 +0300

    test: restructure httpClient Pact test

--- packages/client/__tests__/utilities/httpClient.test.ts
+++ packages/client/__tests__/utilities/httpClient.test.ts
@@ -10,27 +10,25 @@ const provider = new PactV3({
   provider: 'urlShortener-server',
 });
 
-describe('Get api/', () => {
-  test('returns HTTP 200 with a greeting', () => {
-    const greeting = 'hello world';
-    const path = '/api';
-    provider
-      .uponReceiving('a request for a greeting')
-      .withRequest({
-        method: 'GET',
-        path,
-        headers: { Accept: '*/*' },
-      })
-      .willRespondWith({
-        status: 200,
-        contentType: 'application/json',
-        body: { greeting: string(greeting) },
-      });
-
-    return provider.executeTest(async (mockServer) => {
-      process.env.NEXT_PUBLIC_API_BASE_URL = mockServer.url;
-      const greeting = await fetch(path);
-      expect(greeting).toBe(greeting);
+test('Get api/ returns HTTP 200 with a greeting', () => {
+  const greeting = 'hello world';
+  const path = '/api';
+  provider
+    .uponReceiving('a request for a greeting')
+    .withRequest({
+      method: 'GET',
+      path,
+      headers: { Accept: '*/*' },
+    })
+    .willRespondWith({
+      status: 200,
+      contentType: 'application/json',
+      body: { greeting: string(greeting) },
     });
+
+  return provider.executeTest(async (mockServer) => {
+    process.env.NEXT_PUBLIC_API_BASE_URL = mockServer.url;
+    const greeting = await fetch(path);
+    expect(greeting).toBe(greeting);
   });
 });
