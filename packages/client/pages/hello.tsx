507b27a2deaf055d28169b9b716a2810ba8b324a
nahom19 <mailtonahom2@gmail.com>
2023-05-31 06:55:52 +0300

    refactor: rename hello page default function

--- packages/client/pages/hello.tsx
+++ packages/client/pages/hello.tsx
@@ -1,7 +1,7 @@
 import { fetch } from '@/utilities/httpClient';
 import { useEffect, useState } from 'react';
 
-export default function Home() {
+export default function Hello() {
   const [greeting, setGreeting] = useState('');
   const [failure, setFailure] = useState('');
 
