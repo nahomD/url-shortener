f6c878720dae7d581ba872f614e88d0391792499
nahom19 <mailtonahom2@gmail.com>
2023-06-12 13:21:40 +0300

    new: make HOST environment variable required

--- packages/server/src/adapter-restapi-express/main.ts
+++ packages/server/src/adapter-restapi-express/main.ts
@@ -1,6 +1,8 @@
 import app from './app';
 
-const host = process.env.HOST ?? 'localhost';
+if (!process.env.HOST) throw new Error('HOST environment variable is required');
+
+const host = process.env.HOST;
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
 app.listen(port, host, () => {
