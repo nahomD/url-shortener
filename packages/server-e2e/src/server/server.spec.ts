b069b86d37b5143d805d6c2b33003dc3b95b8f73
nahom19 <mailtonahom2@gmail.com>
2023-06-12 15:48:58 +0300

    test: add e2e test for post url

--- packages/server-e2e/src/server/server.spec.ts
+++ packages/server-e2e/src/server/server.spec.ts
@@ -8,3 +8,21 @@ describe('GET /', () => {
     expect(res.data).toEqual({ greeting: 'Hello API' });
   });
 });
+
+function assertHttps(url: string) {
+  const u = new URL(url);
+  expect(u.protocol).toBe('https:');
+}
+
+describe('POST /api/urls', () => {
+  it('should return 201 created for a newly shortened url', async () => {
+    const longUrl = 'https://google.com';
+
+    const res = await axios.post('/api/urls', { url: longUrl });
+
+    expect(res.status).toBe(201);
+    expect(res.data.longUrl).toBe(longUrl);
+    expect(res.data.longUrl).not.toEqual(res.data.shortUrl);
+    assertHttps(res.data.shortUrl);
+  });
+});
